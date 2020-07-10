8beb11c7f2037d4ba5a69586cf541da837d8c8f2
khoelscher <49853042+khoelscher@users.noreply.github.com>
2020-07-10 19:49:05 -0400

    add files via upload
    clean up display for weather api and make it responsive. Add description texts to sections.

--- app.js
+++ app.js
@@ -4,22 +4,35 @@ var userCity = document.querySelector('.displayCity');
 var temp = document.querySelector('.displayTemp');
 var desc = document.querySelector('.displayDesc');
 var img = document.getElementById('weatherIcon');
-button.addEventListener('click', function(name){
+
+
+// CAPTIALIZE FIRST CHARACTER OF STRING-------------
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+// CALL API ON SUBMIT CLICK-------------------------
+button.addEventListener('click', function (name) {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city.value + '&units=imperial&appid=a2e114cdb60f2f87e8096d408c845afd')
-    .then(response => response.json())
-    .then(data => {
-        var tempValue = data['main']['temp'];
-        var nameValue = data['name'];
-        var descValue = data['weather'][0]['description'];
-        var icon = data['weather'][0]['icon'];
 
-        var roundTemp = Math.floor(tempValue);
+        // TURN RESPONSE TO JSON AND RETRIEVE DATA----------
+        .then(response => response.json())
+        .then(data => {
+            var tempValue = data['main']['temp'];
+            var nameValue = data['name'];
+            var descValue = data['weather'][0]['description'];
+            var icon = data['weather'][0]['icon'];
 
-        userCity.innerHTML = nameValue;
-        temp.innerHTML = roundTemp + "°";
-        desc.innerHTML = descValue;
-        img.src = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-    })
-    .catch(err => alert("Could not find that city! Try again!"));
+            // TURN TEMP TO ROUND NUMBER-----------------------
+            var roundTemp = Math.floor(tempValue);
+
+            var upperDesc = capitalize(descValue);
+            // CHANGE HTML WITH DATA INFO----------------------
+            userCity.innerHTML = nameValue;
+            temp.innerHTML = roundTemp + "°";
+            desc.innerHTML = upperDesc;
+            img.src = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+        })
+
+        .catch(err => console.log(err));
 })
-
